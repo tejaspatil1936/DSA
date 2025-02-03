f5ea93135f8ab60f29e898a8187f833638f665da
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-04 00:00:54 +0530

    first commit

---
+++ d0p0_TwoSum.cpp
@@ -0,0 +1,44 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> numMap; 
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i]; 
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i}; 
+            }
+            numMap[nums[i]] = i; 
+        }
+        return {}; 
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    
+    vector<int> result = solution.twoSum(nums, target);
+    cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    
+    return 0;
+}
