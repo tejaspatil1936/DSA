a748bb12e5b358abb683a23589563c2d13cba1ad
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-09 23:25:43 +0530

    solved Zigzag Conversion

--- LeetCode/d5p0ZigzagConversion.cpp
+++ LeetCode/d5p0ZigzagConversion.cpp
@@ -4,23 +4,29 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string convert(string s, int numRows) {
-        if (numRows == 1 || s.length() <= numRows) return s;
+    string convert(string s, int numRows)
+    {
+        if (numRows == 1 || s.length() <= numRows)
+            return s;
 
         vector<string> rows(min(numRows, (int)s.length()));
         int currentRow = 0;
         bool goingDown = false;
 
-        for (char c : s) {
+        for (char c : s)
+        {
             rows[currentRow] += c;
-            if (currentRow == 0 || currentRow == numRows - 1) goingDown = !goingDown;
+            if (currentRow == 0 || currentRow == numRows - 1)
+                goingDown = !goingDown;
             currentRow += goingDown ? 1 : -1;
         }
 
         string result;
-        for (const string& row : rows) {
+        for (const string &row : rows)
+        {
             result += row;
         }
 
@@ -28,11 +34,12 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     Solution sol;
     string s;
     int numRows;
-    
+
     cout << "Enter string: ";
     cin >> s;
     cout << "Enter number of rows: ";
