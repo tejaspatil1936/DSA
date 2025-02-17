c64e8984ff89198a65aca18dfe4b80fde75d92ec
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-17 23:49:50 +0530

    solved longest common prefix

--- LeetCode/d13p0LongestCommonPrefix.cpp
+++ LeetCode/d13p0LongestCommonPrefix.cpp
@@ -5,36 +5,41 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";
-        
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        if (strs.empty())
+            return "";
+
         sort(strs.begin(), strs.end()); // Sort the array
-        
+
         string first = strs[0];
         string last = strs[strs.size() - 1];
         int i = 0;
-        
-        while (i < first.size() && first[i] == last[i]) {
+
+        while (i < first.size() && first[i] == last[i])
+        {
             i++;
         }
-        
+
         return first.substr(0, i);
     }
 };
 
-int main() {
+int main()
+{
     Solution sol;
     vector<vector<string>> testCases = {
         {"flower", "flow", "flight"},
         {"dog", "racecar", "car"},
         {"apple", "ape", "april"},
-        {"",""},
-        {"a"}
-    };
-    
-    for (int i = 0; i < testCases.size(); i++) {
+        {"", ""},
+        {"a"}};
+
+    for (int i = 0; i < testCases.size(); i++)
+    {
         cout << "Test Case " << i + 1 << ": ";
         cout << sol.longestCommonPrefix(testCases[i]) << endl;
     }
