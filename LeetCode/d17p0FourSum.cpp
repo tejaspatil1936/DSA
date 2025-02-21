30ec92aa81fc687ab751eede92538e5ce41e93ef
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-21 21:56:50 +0530

    solved FourSum

--- LeetCode/d17p0FourSum.cpp
+++ LeetCode/d17p0FourSum.cpp
@@ -4,36 +4,49 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> fourSum(vector<int> &nums, int target)
+    {
         vector<vector<int>> result;
         int n = nums.size();
-        if (n < 4) return result;
+        if (n < 4)
+            return result;
 
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < n - 3; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
 
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            for (int j = i + 1; j < n - 2; j++)
+            {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
 
                 int left = j + 1, right = n - 1;
-                while (left < right) {
+                while (left < right)
+                {
                     long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
-                    
-                    if (sum == target) {
+
+                    if (sum == target)
+                    {
                         result.push_back({nums[i], nums[j], nums[left], nums[right]});
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        while (left < right && nums[left] == nums[left + 1])
+                            left++;
+                        while (left < right && nums[right] == nums[right - 1])
+                            right--;
                         left++;
                         right--;
-                    } 
-                    else if (sum < target) {
+                    }
+                    else if (sum < target)
+                    {
                         left++;
-                    } 
-                    else {
+                    }
+                    else
+                    {
                         right--;
                     }
                 }
@@ -44,15 +57,18 @@ public:
 };
 
 // Example usage
-int main() {
+int main()
+{
     Solution sol;
     vector<int> nums = {1, 0, -1, 0, -2, 2};
     int target = 0;
     vector<vector<int>> result = sol.fourSum(nums, target);
 
-    for (const auto& quad : result) {
+    for (const auto &quad : result)
+    {
         cout << "[";
-        for (int num : quad) {
+        for (int num : quad)
+        {
             cout << num << " ";
         }
         cout << "] ";
