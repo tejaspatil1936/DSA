9ace8d4221e14bfcb9d75af34dc744c175eb85bb
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-23 22:07:28 +0530

    solved is valid parenthesis

--- LeetCode/d19p0IsValidParenthesis.cpp
+++ LeetCode/d19p0IsValidParenthesis.cpp
@@ -3,19 +3,27 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool isValid(string s) {
+    bool isValid(string s)
+    {
         stack<char> st;
-        for (char c : s) {
-            if (c == '(' || c == '{' || c == '[') {
+        for (char c : s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
                 st.push(c);
-            } else {
-                if (st.empty()) return false;
+            }
+            else
+            {
+                if (st.empty())
+                    return false;
                 char top = st.top();
-                if ((c == ')' && top != '(') || 
-                    (c == '}' && top != '{') || 
-                    (c == ']' && top != '[')) {
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '['))
+                {
                     return false;
                 }
                 st.pop();
@@ -25,11 +33,12 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     Solution sol;
-    cout << sol.isValid("()") << endl;      // Output: 1 (true)
-    cout << sol.isValid("()[]{}") << endl;  // Output: 1 (true)
-    cout << sol.isValid("(]") << endl;      // Output: 0 (false)
-    cout << sol.isValid("([)]") << endl;    // Output: 0 (false)
+    cout << sol.isValid("()") << endl;     
+    cout << sol.isValid("()[]{}") << endl; 
+    cout << sol.isValid("(]") << endl;     
+    cout << sol.isValid("([)]") << endl;   
     return 0;
 }
