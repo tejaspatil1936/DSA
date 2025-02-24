2af304c2b403cf17bb607fb5255ae21dda0bc973
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-24 21:47:41 +0530

    solved Linked List

---
+++ LeetCode/d20p0MergeLinkedList.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        ListNode *dummy = new ListNode(0);
+        ListNode *current = dummy;
+
+        while (list1 && list2)
+        {
+            if (list1->val < list2->val)
+            {
+                current->next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                current->next = list2;
+                list2 = list2->next;
+            }
+            current = current->next;
+        }
+
+        current->next = list1 ? list1 : list2;
+
+        return dummy->next;
+    }
+};
+
+void printList(ListNode *head)
+{
+    while (head)
+    {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+
+// Example usage
+int main()
+{
+    ListNode *list1 = new ListNode(1);
+    list1->next = new ListNode(2);
+    list1->next->next = new ListNode(4);
+
+    ListNode *list2 = new ListNode(1);
+    list2->next = new ListNode(3);
+    list2->next->next = new ListNode(4);
+
+    Solution sol;
+    ListNode *mergedList = sol.mergeTwoLists(list1, list2);
+
+    printList(mergedList); 
+
+    return 0;
+}
