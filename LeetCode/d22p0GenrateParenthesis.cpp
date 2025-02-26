d90d8b270e861ec46da49c0fc00603229845ba88
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-26 12:00:00 +0530

    solved

--- LeetCode/d22p0GenrateParenthesis.cpp
+++ LeetCode/d22p0GenrateParenthesis.cpp
@@ -3,27 +3,33 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void backtrack(int openCount, int closeCount, int n, string current, vector<string>& result) {
+    void backtrack(int openCount, int closeCount, int n, string current, vector<string> &result)
+    {
         // Base case: If the length of the current string is 2*n, add to result
-        if (current.length() == 2 * n) {
+        if (current.length() == 2 * n)
+        {
             result.push_back(current);
             return;
         }
 
         // Add '(' if we haven't used all 'n' open parentheses
-        if (openCount < n) {
+        if (openCount < n)
+        {
             backtrack(openCount + 1, closeCount, n, current + "(", result);
         }
 
         // Add ')' if closeCount is less than openCount
-        if (closeCount < openCount) {
+        if (closeCount < openCount)
+        {
             backtrack(openCount, closeCount + 1, n, current + ")", result);
         }
     }
 
-    vector<string> generateParenthesis(int n) {
+    vector<string> generateParenthesis(int n)
+    {
         vector<string> result;
         backtrack(0, 0, n, "", result);
         return result;
@@ -31,13 +37,15 @@ public:
 };
 
 // **Driver Code**
-int main() {
+int main()
+{
     Solution sol;
     int n = 3;
     vector<string> result = sol.generateParenthesis(n);
 
     cout << "Generated Parentheses for n = " << n << ":\n";
-    for (const string& s : result) {
+    for (const string &s : result)
+    {
         cout << s << endl;
     }
 
