6510915dd0a64d086c49c0941fa95a3cc69673c3
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-27 22:33:21 +0530

    solved dsa

--- LeetCode/d23p0MergeKList.cpp
+++ LeetCode/d23p0MergeKList.cpp
@@ -4,38 +4,47 @@
 
 using namespace std;
 
-struct ListNode {
+struct ListNode
+{
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
 // Comparator for min-heap
-struct Compare {
-    bool operator()(ListNode* a, ListNode* b) {
+struct Compare
+{
+    bool operator()(ListNode *a, ListNode *b)
+    {
         return a->val > b->val;
     }
 };
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, Compare> minHeap;
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        priority_queue<ListNode *, vector<ListNode *>, Compare> minHeap;
 
-        for (ListNode* list : lists) {
-            if (list) minHeap.push(list);
+        for (ListNode *list : lists)
+        {
+            if (list)
+                minHeap.push(list);
         }
 
         ListNode dummy(0);
-        ListNode* tail = &dummy;
+        ListNode *tail = &dummy;
 
-        while (!minHeap.empty()) {
-            ListNode* minNode = minHeap.top();
+        while (!minHeap.empty())
+        {
+            ListNode *minNode = minHeap.top();
             minHeap.pop();
             tail->next = minNode;
             tail = tail->next;
 
-            if (minNode->next) {
+            if (minNode->next)
+            {
                 minHeap.push(minNode->next);
             }
         }
@@ -45,13 +54,16 @@ public:
 };
 
 // Function to create a linked list from a vector
-ListNode* createLinkedList(const vector<int>& values) {
-    if (values.empty()) return nullptr;
+ListNode *createLinkedList(const vector<int> &values)
+{
+    if (values.empty())
+        return nullptr;
 
-    ListNode* head = new ListNode(values[0]);
-    ListNode* current = head;
+    ListNode *head = new ListNode(values[0]);
+    ListNode *current = head;
 
-    for (size_t i = 1; i < values.size(); ++i) {
+    for (size_t i = 1; i < values.size(); ++i)
+    {
         current->next = new ListNode(values[i]);
         current = current->next;
     }
@@ -60,26 +72,31 @@ ListNode* createLinkedList(const vector<int>& values) {
 }
 
 // Function to print a linked list
-void printLinkedList(ListNode* head) {
-    while (head) {
+void printLinkedList(ListNode *head)
+{
+    while (head)
+    {
         cout << head->val;
-        if (head->next) cout << " -> ";
+        if (head->next)
+            cout << " -> ";
         head = head->next;
     }
     cout << endl;
 }
 
 // Driver code
-int main() {
+int main()
+{
     vector<vector<int>> inputLists = {{1, 4, 5}, {1, 3, 4}, {2, 6}};
-    vector<ListNode*> lists;
+    vector<ListNode *> lists;
 
-    for (const auto& values : inputLists) {
+    for (const auto &values : inputLists)
+    {
         lists.push_back(createLinkedList(values));
     }
 
     Solution solution;
-    ListNode* mergedHead = solution.mergeKLists(lists);
+    ListNode *mergedHead = solution.mergeKLists(lists);
 
     cout << "Merged Linked List: ";
     printLinkedList(mergedHead);
