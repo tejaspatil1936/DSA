079661e1edf49c3820580dc10ea5d017733ccc9f
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-28 21:46:22 +0530

    solved

--- LeetCode/d24p0SwapNodesInPair.cpp
+++ LeetCode/d24p0SwapNodesInPair.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- */
+
 struct ListNode
 {
     int val;
@@ -13,7 +11,6 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-// Solution class with swapPairs function
 class Solution
 {
 public:
@@ -28,12 +25,10 @@ public:
             ListNode *first = head;
             ListNode *second = head->next;
 
-            // Swapping adjacent nodes
             prev->next = second;
             first->next = second->next;
             second->next = first;
 
-            // Move pointers forward
             prev = first;
             head = first->next;
         }
@@ -42,7 +37,6 @@ public:
     }
 };
 
-// Function to create a linked list from an array
 ListNode *createLinkedList(int arr[], int size)
 {
     if (size == 0)
@@ -60,7 +54,6 @@ ListNode *createLinkedList(int arr[], int size)
     return head;
 }
 
-// Function to print a linked list
 void printLinkedList(ListNode *head)
 {
     while (head)
@@ -71,22 +64,18 @@ void printLinkedList(ListNode *head)
     cout << "NULL" << endl;
 }
 
-// Driver function
 int main()
 {
     int arr[] = {1, 2, 3, 4};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    // Create linked list
     ListNode *head = createLinkedList(arr, size);
     cout << "Original List: ";
     printLinkedList(head);
 
-    // Swap pairs
     Solution solution;
     ListNode *swappedHead = solution.swapPairs(head);
 
-    // Print swapped list
     cout << "Swapped List: ";
     printLinkedList(swappedHead);
 
