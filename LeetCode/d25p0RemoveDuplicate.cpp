497a99cd720bf404aa8cc4ff84eca007c8da0062
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-03-03 00:24:16 +0530

    remove duplicate

--- LeetCode/d25p0RemoveDuplicate.cpp
+++ LeetCode/d25p0RemoveDuplicate.cpp
@@ -3,34 +3,40 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0; 
+    int removeDuplicates(vector<int> &nums)
+    {
+        if (nums.empty())
+            return 0;
 
-        int i = 0; 
-        
-        for (int j = 1; j < nums.size(); j++) {
-            if (nums[j] != nums[i]) { 
-                i++; 
-                nums[i] = nums[j]; 
+        int i = 0;
+
+        for (int j = 1; j < nums.size(); j++)
+        {
+            if (nums[j] != nums[i])
+            {
+                i++;
+                nums[i] = nums[j];
             }
         }
-        
-        return i + 1; 
+
+        return i + 1;
     }
 };
 
-int main() {
+int main()
+{
     Solution sol;
     vector<int> nums = {1, 1, 2, 2, 3, 4, 4, 5};
 
     int k = sol.removeDuplicates(nums);
 
-    
     cout << "Unique elements count: " << k << endl;
     cout << "Modified array: ";
-    for (int i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++)
+    {
         cout << nums[i] << " ";
     }
     cout << endl;
