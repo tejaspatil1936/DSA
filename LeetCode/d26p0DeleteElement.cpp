44a563a5cc10d6918500f864771d9cee74f46c73
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-03-03 20:59:50 +0530

    remove elements

--- LeetCode/d26p0DeleteElement.cpp
+++ LeetCode/d26p0DeleteElement.cpp
@@ -3,12 +3,16 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int removeElement(vector<int>& nums, int val) {
+    int removeElement(vector<int> &nums, int val)
+    {
         int i = 0;
-        for (int j = 0; j < nums.size(); j++) {
-            if (nums[j] != val) {
+        for (int j = 0; j < nums.size(); j++)
+        {
+            if (nums[j] != val)
+            {
                 nums[i] = nums[j];
                 i++;
             }
@@ -17,7 +21,8 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     Solution sol;
     vector<int> nums = {3, 2, 2, 3};
     int val = 3;
@@ -26,7 +31,8 @@ int main() {
 
     cout << "Elements count after removal: " << k << endl;
     cout << "Modified array: ";
-    for (int i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++)
+    {
         cout << nums[i] << " ";
     }
     cout << endl;
